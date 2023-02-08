5d756587c098f83c9bf28646faa2d4b7218b45de
Greathoney <thyoon48@gmail.com>
2023-02-08 22:09:51 +0900

    사람대 사람 오목 프로그램 완성

--- gomoku.py
+++ gomoku.py
@@ -17,6 +17,7 @@ BOARD_SHAPE = (STONE_NUM, STONE_NUM)
 LINE_GAP = int(BOARD_SIZE / STONE_NUM)  # pygame 그릴 때, 격자 간격 (pixel)
 DOT_SIZE = 4
 
+
 class Gomoku:
     def __init__(self):
         self.board = np.zeros(BOARD_SHAPE)
@@ -29,25 +30,27 @@ class Gomoku:
     def step(self, action):  # action: (x, y)
         if self.board[action] != 0:
             raise ValueError('Invalid action')
-        
-        new_board = self.board.copy()
-        new_board[action] = self.turn
-        self.board_history.append(new_board)
 
-        reward = self.judge()*self.turn
+        next_board = self.board.copy()
+        next_board[action] = self.turn
+        self.board_history.append(next_board)
+
+        self.board = next_board
+        # reward = self.judge() * self.turn
+        reward = 0
+        self.done = self.is_finish(action)
 
         self.turn *= -1
-        self.board = new_board
-        return self.board, reward
+        return self.board, reward, self.done
 
     def judge(self):
-        
+
         #5개 연속으로 놓여있는지 확인
         #가로
         for i in range(STONE_NUM):
-
-        #3개 연속으로 놓여있는지 확인
-        #가로
+            pass
+            #3개 연속으로 놓여있는지 확인
+            #가로
         for i in range(STONE_NUM):
             for j in range(STONE_NUM-4):
                 if np.all(self.board[i:i+4][j]) == self.turn:
@@ -66,64 +69,101 @@ class Gomoku:
             for j in range(4, STONE_NUM):
                 if self.board[i][j] == self.board[i+1][j-1] == self.board[i+2][j-2] == self.board[i+3][j-3] == self.board[i+4][j-4] == self.turn:
                     return self.turn
+
+    def check_five_in_a_row(self, stone, direction, count):
+        if count == 5:
+            return True
         
-            
+        next_stone = (stone[0]+direction[0], stone[1]+direction[1])
+
+        if next_stone[0] < 0 or next_stone[0] >= STONE_NUM or next_stone[1] < 0 or next_stone[1] >= STONE_NUM:
+            return False
+
+        if self.board[next_stone] == self.turn:
+            return self.check_five_in_a_row(next_stone, direction, count+1)
+        else:
+            return False
+
+    def is_finish(self, last_stone):
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1),
+                      (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        for direction in directions:
+            if self.check_five_in_a_row(last_stone, direction, 1):
+                return True
+        else:
+            return False
+
+    # pygame 초기화
     def pygame_init(self):
         pygame.init()
         self.screen = pygame.display.set_mode(WINDOW_SIZE)
         pygame.display.set_caption('Gomoku')
         pygame.display.flip()
 
-
+    # pygame 렌더
     def render(self):
         self.screen.fill(WHITE)
 
         # draw line
         for i in range(15):
-            pygame.draw.line(self.screen, BLACK, (0, i*LINE_GAP) + BOARD_MARGIN, (BOARD_SIZE-LINE_GAP, i*LINE_GAP) + BOARD_MARGIN)
-            pygame.draw.line(self.screen, BLACK, (i*LINE_GAP, 0) + BOARD_MARGIN, (i*LINE_GAP, BOARD_SIZE-LINE_GAP) + BOARD_MARGIN)
-        
+            pygame.draw.line(self.screen, BLACK, (0, i*LINE_GAP) + BOARD_MARGIN,
+                             (BOARD_SIZE-LINE_GAP, i*LINE_GAP) + BOARD_MARGIN)
+            pygame.draw.line(self.screen, BLACK, (i*LINE_GAP, 0) + BOARD_MARGIN,
+                             (i*LINE_GAP, BOARD_SIZE-LINE_GAP) + BOARD_MARGIN)
+
         # draw dot
         for i in range(3):
             for j in range(3):
-                pygame.draw.circle(self.screen, BLACK, (i*4*LINE_GAP, j*4*LINE_GAP) + BOARD_MARGIN + (LINE_GAP*3, LINE_GAP*3), DOT_SIZE)
+                pygame.draw.circle(self.screen, BLACK, (i*4*LINE_GAP, j*4*LINE_GAP) +
+                                   BOARD_MARGIN + (LINE_GAP*3, LINE_GAP*3), DOT_SIZE)
 
         # draw stone
         for i in range(15):
             for j in range(15):
                 if self.board[i][j] == 1:
-                    pygame.draw.circle(self.screen, BLACK, (i*LINE_GAP, j*LINE_GAP) + BOARD_MARGIN, int(LINE_GAP/2))
+                    pygame.draw.circle(
+                        self.screen, BLACK, (i*LINE_GAP, j*LINE_GAP) + BOARD_MARGIN, int(LINE_GAP/2.5))
 
                 elif self.board[i][j] == -1:
-                    pygame.draw.circle(self.screen, WHITE, (i*LINE_GAP, j*LINE_GAP) + BOARD_MARGIN, int(LINE_GAP/2))
-                    pygame.draw.circle(self.screen, BLACK, (i*LINE_GAP, j*LINE_GAP) + BOARD_MARGIN, int(LINE_GAP/2), 1)
+                    pygame.draw.circle(
+                        self.screen, WHITE, (i*LINE_GAP, j*LINE_GAP) + BOARD_MARGIN, int(LINE_GAP/2.5))
+                    pygame.draw.circle(
+                        self.screen, BLACK, (i*LINE_GAP, j*LINE_GAP) + BOARD_MARGIN, int(LINE_GAP/2.5), 1)
 
-        # say turn
+        # turn 표시
         if self.turn == 1:
             text = 'Black turn'
         else:
             text = 'White turn'
+
         font = pygame.font.SysFont('comicsansms', 30)
         text = font.render(text, True, BLACK)
-        self.screen.blit(text, (BOARD_SIZE + BOARD_MARGIN[0] + 50, BOARD_MARGIN[1]))
-
-        pygame.display.flip()
+        self.screen.blit(
+            text, (BOARD_SIZE + BOARD_MARGIN[0] + 50, BOARD_MARGIN[1]))
         
+        # 다른 클래스 상속받아서 사용할 때는 pygame.display.flip()을 호출하지 않음
+        if type(self) == Gomoku:
+            pygame.display.flip()
 
+
+# 자동으로 돌을 두는 예제 실행
 if __name__ == '__main__':
     gomoku = Gomoku()
     gomoku.render()
+    time.sleep(1)
+
     gomoku.step((1, 1))
+    gomoku.render()
     time.sleep(1)
 
-    gomoku.render()
     gomoku.step((2, 2))
+    gomoku.render()
     time.sleep(1)
 
-    gomoku.render()
     gomoku.step((3, 3))
+    gomoku.render()
     time.sleep(1)
 
-    gomoku.render()
     gomoku.step((4, 4))
+    gomoku.render()
     time.sleep(1)
---
+++ gomokuhumanvshuman.py
@@ -0,0 +1,69 @@
+from gomoku import *
+
+import sys
+import pygame
+
+FPS = 10
+
+class GomokuHumanVsHuman(Gomoku):
+    def render(self):
+        super().render()
+
+        # 마우스 커서 위치에 힌트 표시
+        x, y = pygame.mouse.get_pos()
+        x = int(((x+LINE_GAP/2) - BOARD_MARGIN[0]) / LINE_GAP)
+        y = int(((y+LINE_GAP/2) - BOARD_MARGIN[1]) / LINE_GAP)
+
+        # 마우스 커서 위치가 보드 안에 있을 때만 힌트 표시
+        if 0 <= x < STONE_NUM and 0 <= y < STONE_NUM:
+            if self.board[x, y] == 0:
+                pygame.draw.circle(self.screen, (0, 0, 0), (x * LINE_GAP +
+                                   BOARD_MARGIN[0], y * LINE_GAP + BOARD_MARGIN[1]), int(LINE_GAP/2.5), 1)
+
+        # 게임 종료 시 결과 표시
+        if self.done * self.turn == -1:
+            # pygame print
+            font = pygame.font.SysFont('comicsansms', 30)
+            text = font.render('Black win', True, BLACK)
+            gomoku.screen.blit(
+                text, (BOARD_SIZE + BOARD_MARGIN[0] + 50, BOARD_MARGIN[1] + 50))
+
+        elif self.done * self.turn == 1:
+            # pygame print
+            font = pygame.font.SysFont('comicsansms', 30)
+            text = font.render('White win', True, BLACK)
+            gomoku.screen.blit(
+                text, (BOARD_SIZE + BOARD_MARGIN[0] + 50, BOARD_MARGIN[1] + 50))
+        else:
+            # pygame print
+            font = pygame.font.SysFont('comicsansms', 30)
+            text = font.render('Playing...', True, BLACK)
+            gomoku.screen.blit(
+                text, (BOARD_SIZE + BOARD_MARGIN[0] + 50, BOARD_MARGIN[1] + 50))
+        pygame.display.flip()
+
+
+gomoku = GomokuHumanVsHuman()
+
+while True:
+
+    # 이벤트 처리
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+        # 마우스 클릭 시 돌 놓기
+        if event.type == pygame.MOUSEBUTTONDOWN and not gomoku.done:
+            x, y = event.pos
+            x = int(((x+LINE_GAP/2) - BOARD_MARGIN[0]) / LINE_GAP)
+            y = int(((y+LINE_GAP/2) - BOARD_MARGIN[1]) / LINE_GAP)
+            if 0 <= x < STONE_NUM and 0 <= y < STONE_NUM:
+                if gomoku.board[x, y] == 0:
+                    gomoku.step((x, y))
+
+    gomoku.render()
+
+    # 프레임 속도 조절
+    clock = pygame.time.Clock()
+    clock.tick(FPS)
